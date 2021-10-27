ac948c0623ff1c89c2eee0b666c0646fae8dd0b9
fff <fff@fff.com>
2021-10-27 08:53:58 +0800

    feat:init project

--- gulimall-coupon/src/main/java/com/atguigu/gulimall/coupon/controller/CouponController.java
+++ gulimall-coupon/src/main/java/com/atguigu/gulimall/coupon/controller/CouponController.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,11 +26,32 @@ import com.atguigu.common.utils.R;
  * @email fff@gmail.com
  * @date 2021-10-21 10:35:07
  */
+@RefreshScope
 @RestController
 @RequestMapping("coupon/coupon")
 public class CouponController {
     @Autowired
     private CouponService couponService;
+
+    @Value("${coupon.user.name}")
+    String userName;
+    @Value("${coupon.user.age}")
+    String age;
+
+    @RequestMapping("/test")
+    public R test(){
+        return R.ok().put("name",userName).put("age",age);
+    }
+
+    @RequestMapping("/member/list")
+    public R memberCoupons(){
+        CouponEntity couponEntity = new CouponEntity();
+        couponEntity.setCouponName("测试满100-50");
+
+        return R.ok().put("coupons",couponEntity);
+
+    }
+
 
     /**
      * 列表
--- gulimall-member/src/main/java/com/atguigu/gulimall/member/controller/MemberController.java
+++ gulimall-member/src/main/java/com/atguigu/gulimall/member/controller/MemberController.java
@@ -3,6 +3,7 @@
 import java.util.Arrays;
 import java.util.Map;
 
+import com.atguigu.gulimall.member.feign.CouponFeignService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -29,6 +30,20 @@ import com.atguigu.common.utils.R;
 public class MemberController {
     @Autowired
     private MemberService memberService;
+
+    @Autowired
+    CouponFeignService couponFeignService;
+
+    @RequestMapping("coupons")
+    public R test(){
+        MemberEntity memberEntity = new MemberEntity();
+        memberEntity.setNickname("test昵称");
+
+       R couponsR = couponFeignService.memberCoupons();
+
+        return R.ok().put("member",memberEntity).put("coupons",couponsR.get("coupons"));
+    }
+
 
     /**
      * 列表
